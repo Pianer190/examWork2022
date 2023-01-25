5f44ade4bd478490255d8ba763a8e707f0098f75
Dmitry <pianer190@mail.ru>
2023-01-25 09:50:55 +0300

    Написана программа в соответствии с заданием.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] strings = new string[4]{"hello","my","world",";-)"};
+List<string> output = new List<string>();
+
+for(int i = 0; i < strings.Count(); i++){
+    if(strings[i].Length > 3) continue;
+    output.Add(strings[i]);
+}
+
+foreach(string str in output){
+    Console.WriteLine(str);
+}
